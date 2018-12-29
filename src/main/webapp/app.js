4bde473bada64f831918f2d3f70f1a81aff0a5c8
Matthias Goulley <mattgoulley@gmail.com>
2018-12-29 16:04:51 +0100

    Fix infinite loop

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -8,7 +8,6 @@ var app = angular.module('plunker', []).controller('Tinytwitter', ['$scope', '$w
       }).execute(
         function(resp) {
           console.log(resp);
-          $window.alert(resp.content);
         });
     }
 
